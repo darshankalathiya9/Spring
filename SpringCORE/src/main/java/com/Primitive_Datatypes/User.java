024698c825593bc2662cf5e5170f0acf10f6f9cc
darshankalathiya9 <darshankalathiya963@gmail.com>
2023-05-05 21:47:24 +0530

    Spring Framework Iniatlization

---
+++ SpringCORE/src/main/java/com/Primitive_Datatypes/User.java
@@ -0,0 +1,48 @@
+package com.Primitive_Datatypes;
+
+public class User {
+	private int ID;
+	private String Name;
+	private long Contact;
+
+	public User() {
+		super();
+	}
+
+	public User(int iD, String name, long contact) {
+		super();
+		ID = iD;
+		Name = name;
+		Contact = contact;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public long getContact() {
+		return Contact;
+	}
+
+	public void setContact(long contact) {
+		Contact = contact;
+	}
+
+	@Override
+	public String toString() {
+		return "User [ID=" + ID + ", Name=" + Name + ", Contact=" + Contact + "]";
+	}
+
+}
