81525f2dfbea9ea8a1aa0eaf8f695e5969106412
darshankalathiya9 <darshankalathiya963@gmail.com>
2023-05-27 17:49:05 +0530

    SpringORM Done.... CRUD Operation Done

--- SpringORM/src/main/java/com/SpringORM/App.java
+++ SpringORM/src/main/java/com/SpringORM/App.java
@@ -1,5 +1,7 @@
 package com.SpringORM;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,9 +12,26 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext apc = new ClassPathXmlApplicationContext("com/SpringORM/Config.xml");
 		StudentDao dao = (StudentDao) apc.getBean("dao");
-		
+
 		/* Insert Student */
-		Student s1 = new Student(333,"Tirthu","Ahmedabad",898035079);
-		dao.insertStudent(s1);
+//		Student s1 = new Student(555,"Guarang","Vatva",123456);
+//		dao.insertStudent(s1);
+
+		/* Update Student */
+//		Student s1 = new Student(555,"Gaurang Dalwadi","Ahmedabad",985421);
+//		dao.updateStudent(s1);
+
+		/* Delete Student */
+//		Student s1 = new Student(444,"Piyush","Ahmedabad",70466881);
+//		dao.deleteStudent(s1);
+
+		/* Single Data Fetch */
+//		Student s1 = (Student) dao.getStudentByID(555);
+//		System.out.println(s1);
+
+		/* Multiple Data Fetch */
+		List<Student> list = dao.getAllStudent();
+		System.out.println(list);
+
 	}
 }
--- SpringORM/src/main/java/com/SpringORM/Dao/StudentDao.java
+++ SpringORM/src/main/java/com/SpringORM/Dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.SpringORM.Dao;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import com.SpringORM.Entity.Student;
@@ -19,5 +21,27 @@ public class StudentDao {
 	@Transactional
 	public void insertStudent(Student s) {
 		this.hibernateTemplate.save(s);
+	}
+
+	/* Update Student */
+	@Transactional
+	public void updateStudent(Student s) {
+		this.hibernateTemplate.update(s);
+	}
+
+	/* Delete Student */
+	@Transactional
+	public void deleteStudent(Student s) {
+		this.hibernateTemplate.delete(s);
+	}
+
+	/* Single Data Fetch */
+	public Student getStudentByID(int ID) {
+		return this.hibernateTemplate.get(Student.class, ID);
+	}
+
+	/* Multiple Data Fetch */
+	public List<Student> getAllStudent() {
+		return this.hibernateTemplate.loadAll(Student.class);
 	}
 }
