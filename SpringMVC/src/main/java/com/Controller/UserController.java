f2465560dc16f5e8a158a333991d50330db421ec
darshankalathiya9 <darshankalathiya963@gmail.com>
2023-06-03 10:15:27 +0530

    Registration Done.... with Model(addAttribute).... EL & JSTL Tag Done

--- SpringMVC/src/main/java/com/Controller/UserController.java
+++ SpringMVC/src/main/java/com/Controller/UserController.java
@@ -1,11 +1,17 @@
 package com.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.Dao.UserDao;
+import com.Model.User;
 
 @Controller
 public class UserController {
@@ -15,6 +21,14 @@ public class UserController {
 	
 	@RequestMapping("/")
 	public String index(Model m) {
+		m.addAttribute("name", "Darshan Kalathiya (DK)");
+		
+		List<String> list = new ArrayList<String>();
+		list.add("DK");
+		list.add("Bhano");
+		list.add("Tirthu");
+		m.addAttribute("list", list);
+		
 		return "index";
 	}
 	
@@ -31,6 +45,14 @@ public class UserController {
 	@RequestMapping("/contact")
 	public String contact() {
 		return "contact";
+	}
+	
+	@RequestMapping(value="/insertUser",method=RequestMethod.POST)
+	public String insertUser(@ModelAttribute User u, Model m) {
+		dao.insertOrUpdateUser(u);
+		List<User> list = dao.getAllUser();
+		m.addAttribute("list",list);
+		return "home";
 	}
 
 }
--- SpringMVC/src/main/webapp/WEB-INF/views/index.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,8 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page isELIgnored="false" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,14 +10,22 @@
 <title>Index Page</title>
 </head>
 <body>
-	<h2>
-		<a href="register">Register....</a>
-	</h2>
-	<h2>
-		<a href="about">About.....</a>
-	</h2>
-	<h2>
-		<a href="contact">Contact.....</a>
-	</h2>
+	<h2><a href="register">Register....</a></h2>
+	<h2><a href="about">About.....</a></h2>
+	<h2><a href="contact">Contact.....</a></h2>
+	
+	<%-- <% String name = (String) request.getAttribute("name"); %>
+	<h2><%=name %></h2> --%>
+	
+	 <h2>${name }</h2> 
+	
+	<%-- <% List<String> list = (List<String>) request.getAttribute("list"); %>
+	<% out.print(list); %> --%>
+	
+	<h2>${list }</h2>
+	<c:forEach items="${list }" var="fr">
+		<h1>${fr }</h1>
+	</c:forEach>
+	
 </body>
 </html>
